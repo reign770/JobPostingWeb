8a6cd787bb96381077fc9456ff090fd8c6ab8275
AoteChou <aotechou@gmail.com>
2013-05-26 20:04:28 +0800

    pageBeanToJsonMapService 用于返回适合datagrid的json格式

---
+++ JobPostingsWeb/src/com/codeblue/service/enterprise/PageBeanToJsonMapService.java
@@ -0,0 +1,10 @@
+package com.codeblue.service.enterprise;
+
+import java.util.Map;
+
+import com.codeblue.util.PageBean;
+
+public interface PageBeanToJsonMapService {
+	
+	public Map convertOfDWATA(PageBean pageBean);
+}
