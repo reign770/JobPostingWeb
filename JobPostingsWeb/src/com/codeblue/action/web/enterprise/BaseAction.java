a03f6f1a25e4760f98989af39d8c0372bb8f6dfc
AoteChou <aotechou@gmail.com>
2013-05-28 23:57:12 +0800

    baseaction的修改

--- JobPostingsWeb/src/com/codeblue/action/web/enterprise/BaseAction.java
+++ JobPostingsWeb/src/com/codeblue/action/web/enterprise/BaseAction.java
@@ -2,14 +2,26 @@ package com.codeblue.action.web.enterprise;
 
 import java.util.Map;
 
-import org.apache.struts2.interceptor.SessionAware;;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.apache.struts2.interceptor.SessionAware;
+import org.apache.struts2.util.ServletContextAware;
+import org.springframework.web.context.ServletConfigAware;
 
 /**
  * @author 周奥特
- * 获取session的基类
+ * 获取session以及各种系统变量的基类
  */
-public class BaseAction implements SessionAware{
+public class BaseAction implements SessionAware,ServletRequestAware,ServletResponseAware,ServletContextAware{
 	protected Map<String, Object> session;
+	protected HttpServletRequest request;
+	protected HttpServletResponse response;
+	protected ServletContext servletContext;
 
 	@Override
 	public void setSession(Map<String, Object> session) {
@@ -17,5 +29,30 @@ public class BaseAction implements SessionAware{
 		this.session=session;
 		
 	}
+
+
+	@Override
+	public void setServletContext(ServletContext arg0) {
+		// TODO Auto-generated method stub
+		this.servletContext=arg0;
+	}
+
+
+
+	@Override
+	public void setServletResponse(HttpServletResponse arg0) {
+		// TODO Auto-generated method stub
+		this.response=arg0;
+	}
+
+
+
+	@Override
+	public void setServletRequest(HttpServletRequest arg0) {
+		// TODO Auto-generated method stub
+		this.request=arg0;
+	}
+	
+
 
 }
