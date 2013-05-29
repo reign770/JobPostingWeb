3897ec553e252e8fc3f4d08f8fca4d32512e974f
AoteChou <aotechou@gmail.com>
2013-05-29 15:42:48 +0800

    为了pull的commit

--- JobPostingsWeb/src/com/codeblue/action/web/enterprise/CreatePositionDetailAction.java
+++ JobPostingsWeb/src/com/codeblue/action/web/enterprise/CreatePositionDetailAction.java
@@ -1,6 +1,8 @@
 package com.codeblue.action.web.enterprise;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -13,18 +15,13 @@ public class CreatePositionDetailAction extends BaseAction {
 	private Recruitment recruitment;
 	@Resource(name="ent_recruitmentService")
 	private RecruitmentService recruitmentService;
+	
+	private Map<String,Object> jsonMap=new HashMap<>();
+	
 	public String load() {
 		return "success";
 	}
-	public String insert() {
-		Enterprise enterprise=(Enterprise)session.get("user");
-		recruitment.setEnterprise(enterprise);
-		System.out.println(recruitment.getIndustry().getIndustryId()+" "+recruitment.getDeadline());
-		recruitment.setState(RecruitmentState.POSTING);
-		recruitment.setReleaseDate(new Date());
-		recruitmentService.insertRecruitment(recruitment);
-		return "success";
-	}
+	
 
 	public Recruitment getRecruitment() {
 		return recruitment;
@@ -32,6 +29,12 @@ public class CreatePositionDetailAction extends BaseAction {
 
 	public void setRecruitment(Recruitment recruitment) {
 		this.recruitment = recruitment;
+	}
+	public Map<String, Object> getJsonMap() {
+		return jsonMap;
+	}
+	public void setJsonMap(Map<String, Object> jsonMap) {
+		this.jsonMap = jsonMap;
 	}
 
 
