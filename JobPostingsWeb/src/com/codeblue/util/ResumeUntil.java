2fe5c1288a6adc72667907f27abdca538cee9151
Li Jiaqi <reign770@gmail.com>
2013-06-02 11:23:05 +0800

    工具类修改

--- JobPostingsWeb/src/com/codeblue/util/ResumeUntil.java
+++ JobPostingsWeb/src/com/codeblue/util/ResumeUntil.java
@@ -8,10 +8,10 @@ public class ResumeUntil {
 	public static Resume changeToResume(Brief brief) {
 		Resume resume = new Resume();
 		resume.setStudentId(brief.getStudentId());
-		resume.setInterests(brief.getInterests().split("$"));
-		resume.setHonors(brief.getHonor().split("$"));
-		resume.setMajorCourse(brief.getMajorCourse().split("$"));
-	    resume.setExperience(brief.getExperience().split("$"));
+		resume.setInterests(brief.getInterests().split(","));
+		resume.setHonors(brief.getHonor().split(","));
+		resume.setMajorCourse(brief.getMajorCourse().split(","));
+	    resume.setExperience(brief.getExperience().split(","));
 	    resume.setResume(brief.getResume());
 		return resume;
 	}
@@ -21,25 +21,25 @@ public class ResumeUntil {
 		StringBuffer majorCoursesBuffer  = new StringBuffer();
 		for(String course:resume.getMajorCourse()) {
 			if(course != null && !course.trim().equals(""))
-				majorCoursesBuffer.append(course).append("$");
+				majorCoursesBuffer.append(course).append(",");
 		}
 		
 		StringBuffer intrestBuffer = new StringBuffer();
 		for(String intrest:resume.getInterests()) {
 			if(intrest!=null&&!intrest.trim().equals(""))
-				intrestBuffer.append(intrest.trim()).append("$");
+				intrestBuffer.append(intrest.trim()).append(",");
 		}
 		
 		StringBuffer honorBuffer = new StringBuffer();
 		for(String honor:resume.getHonors()) {
 			if(honor!=null&&!honor.trim().equals("")){
-				honorBuffer.append(honor.trim()).append("$");
+				honorBuffer.append(honor.trim()).append(",");
 			}
 		}
 		StringBuffer experienceBuffer = new StringBuffer();
 		for(String exper:resume.getHonors()) {
 			if(exper != null && !exper.trim().equals("")){
-				experienceBuffer.append(exper.trim()).append("$");
+				experienceBuffer.append(exper.trim()).append(",");
 			}
 		}
 		
