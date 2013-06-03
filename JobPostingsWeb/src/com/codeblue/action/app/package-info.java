729a5983f4aaa3c2d02cd86e64fa527d65bc86b9
Li Jiaqi <reign770@gmail.com>
2013-06-03 14:15:45 +0800

    删除和转移文件

--- JobPostingsWeb/src/com/codeblue/action/app/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author 李佳奇
- * @datetime 2013-5-20 下午6:05:46
- */
-package com.codeblue.action.app;
--- JobPostingsWeb/src/com/codeblue/vo/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author 李佳奇
- * @datetime 2013-5-20 下午8:18:39
- */
-package com.codeblue.vo;
